9e934ca689c85f4b515c940c3720b13f84c69f9a
Muzamil-Fatima <muzamilzubair987654@gmail.com>
2026-03-17 15:52:07 +0500

    dd sidebar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./scenes/dashboard/Dashboard";
+import Team from "./scenes/team/Team";
+import Contacts from "./scenes/contacts/Contacts";
+import Invoices from "./scenes/invoices/Invoices";
+import Form from "./scenes/form/Form";
+import Pie from "./scenes/pie/Pie";
+import Line from "./scenes/line/Line";
+import Faq from "./scenes/faq/Faq";
+import Calendar from "./scenes/calendar/Calendar";
+import Geography from "./scenes/geography/Geography";
+import TopBar from "./scenes/global/TopBar";
+import SideBar from "./scenes/global/SideBar";
+import Bars from "./scenes/bars/Bars";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <SideBar />
+        <main>
+          <TopBar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/invoices" element={<Invoices />} />
+            <Route path="/form" element={<Form />} />
+            <Route path="/bar" element={<Bars />} />
+            <Route path="/pie" element={<Pie />} />
+            <Route path="/line" element={<Line />} />
+            <Route path="/faq" element={<Faq />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/geography" element={<Geography />} />
+          </Routes>
+        </main>
+      </div>
+    </>
   );
 }
 
---
+++ src/scenes/bars/Bars.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {Box, IconButton, Typography, useTheme} from "@mui/material"
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined"
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined"
+const Bars = () => {
+  return (
+    <div>
+      <Box>
+        <ProSidebar>
+          <Menu>
+            {/* Logo and Menu Icon */}
+            <MenuItem></MenuItem>
+
+            <Box>
+              <Box></Box>
+              <Box>
+                <Typography></Typography>
+                <Typography>VP Fancy Admin</Typography>
+              </Box>
+            </Box>
+
+
+            <Box>
+                <Item title="Dashboard" 
+                to="/"
+                icon={<HomeOutlinedIcon}
+                />
+            </Box>
+          </Menu>
+        </ProSidebar>
+      </Box>
+    </div>
+  );
+};
+
+export default Bars;
